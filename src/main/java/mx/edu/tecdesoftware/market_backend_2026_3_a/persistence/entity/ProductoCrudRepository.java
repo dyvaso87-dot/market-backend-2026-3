af162a455c51d505bab0324601635aeb72ea3d4e
DylanTSW <dylan.vazquez@tecdesoftware.edu.mx>
2026-07-07 08:57:26 -0600

    feat(web): finalize ProductController with full CRUD endpoints

---
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_a/persistence/entity/ProductoCrudRepository.java
@@ -0,0 +1,9 @@
+package mx.edu.tecdesoftware.market_backend_2026_3_a.persistence.entity;
+
+
+import mx.edu.tecdesoftware.market_backend_2026_3_a.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductoCrudRepository
+        extends CrudRepository<Producto, Integer> {
+}
--- src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_a/web/controller/ProductController.java
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_a/web/controller/ProductController.java
@@ -1,9 +1,54 @@
 package mx.edu.tecdesoftware.market_backend_2026_3_a.web.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import mx.edu.tecdesoftware.market_backend_2026_3_a.domain.repository.ProductRepository;
+import mx.edu.tecdesoftware.market_backend_2026_3_a.domain.service.Product;
+import mx.edu.tecdesoftware.market_backend_2026_3_a.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-}
+
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private ProductRepository productRepository;
+    @GetMapping("")
+    public ResponseEntity<List<Product>>getAll() {
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId){
+        return productService.getProduct(productId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoriId") int categoryId){
+        return productService.getByCategory(categoryId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Product> save(@RequestBody Product product){
+        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable("id") int productId) {
+        if (productService.delete(productId)){
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    }
